a64a9c2461906ab4738ea770c2ed7bfd83655701
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-01-17 21:52:14 +0100

    Asciimoji v2

---
+++ asciimoji/example/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/anotherhadi/gml-ui/asciimoji"
+)
+
+func main() {
+	fmt.Println(asciimoji.Bear)
+	fmt.Println(asciimoji.Bearhug)
+	fmt.Println(asciimoji.Flower)
+}
